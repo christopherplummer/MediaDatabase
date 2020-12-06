b15b1178e89eb9d6d517b28b251c12febc428aca
Christopher Plummer <cplummer@reservations.com>
2020-12-06 10:05:20 -0500

    Added abstract EntityService class and interface

---
+++ AnimeDatabase.Web/Services/EntityService.cs
@@ -0,0 +1,31 @@
+﻿using AnimeDatabase.Web.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimeDatabase.Web.Services
+{
+    public abstract class EntityService<TEntity> : IEntityService<TEntity> where TEntity : class
+    {
+        protected readonly IConfiguration _config;
+
+        protected readonly IRestClient _restClient;
+
+        protected EntityService(IRestClient restClient, IConfiguration config)
+        {
+            _config = config;
+            _restClient = restClient;
+        }
+
+        public abstract Task<List<TEntity>> Get();
+
+        //public abstract Task<TEntity> Get(Guid id);
+
+        //public abstract Task<TEntity> Post(TEntity model);
+
+        //public abstract Task<TEntity> Put(TEntity model);
+
+        //public abstract Task<bool> Delete(Guid id);
+    }
+}
---
+++ AnimeDatabase.Web/Services/Interfaces/IEntityService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimeDatabase.Web.Services.Interfaces
+{
+    public interface IEntityService<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> Get();
+
+        //Task<TEntity> Get(Guid id);
+
+        //Task<TEntity> Post(TEntity model);
+
+        //Task<TEntity> Put(TEntity model);
+
+        //Task<bool> Delete(Guid id);
+    }
+}
